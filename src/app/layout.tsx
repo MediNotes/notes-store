6f312de0831967d6e9c05a0740bb4f4cb50ba26d
rahulpyd <rahulpyd@gmail.com>
2024-01-19 05:56:34 +0530

    Project setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,24 +7,24 @@ import SessionProvider from "./SessionProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Flowmazon",
-  description: "We make your wallet cry",
+    title: "Medcipher",
+    description: "Learning made easy",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <SessionProvider>
-          <Navbar />
-          <main className="m-auto min-w-[300px] max-w-7xl p-4">{children}</main>
-          <Footer />
-        </SessionProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <SessionProvider>
+                    <Navbar />
+                    <main className="m-auto min-w-[300px] max-w-7xl p-4">{children}</main>
+                    <Footer />
+                </SessionProvider>
+            </body>
+        </html>
+    );
 }
