bce9ed40b99fd3888b82c04764ec25c6bce3ff44
rahulpyd <rahulpyd@gmail.com>
2024-01-19 06:39:14 +0530

    [fileUrl] create products with fileUrl field 2/n

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -5,73 +5,81 @@ import { redirect } from "next/navigation";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 
 export const metadata = {
-  title: "Add Product - Flowmazon",
+    title: "Add Product - Flowmazon",
 };
 
 async function addProduct(formData: FormData) {
-  "use server";
+    "use server";
 
-  const session = await getServerSession(authOptions);
+    const session = await getServerSession(authOptions);
 
-  if (!session) {
-    redirect("/api/auth/signin?callbackUrl=/add-product");
-  }
+    if (!session) {
+        redirect("/api/auth/signin?callbackUrl=/add-product");
+    }
 
-  const name = formData.get("name")?.toString();
-  const description = formData.get("description")?.toString();
-  const imageUrl = formData.get("imageUrl")?.toString();
-  const price = Number(formData.get("price") || 0);
+    const name = formData.get("name")?.toString();
+    const description = formData.get("description")?.toString();
+    const fileUrl = formData.get("fileUrl")?.toString();
+    const imageUrl = formData.get("imageUrl")?.toString();
+    const price = Number(formData.get("price") || 0);
 
-  if (!name || !description || !imageUrl || !price) {
-    throw Error("Missing required fields");
-  }
+    if (!name || !description || !fileUrl || !imageUrl || !price) {
+        throw Error("Missing required fields");
+    }
 
-  await prisma.product.create({
-    data: { name, description, imageUrl, price },
-  });
+    await prisma.product.create({
+        data: { name, description, fileUrl, imageUrl, price },
+    });
 
-  redirect("/");
+    redirect("/");
 }
 
 export default async function AddProductPage() {
-  const session = await getServerSession(authOptions);
+    const session = await getServerSession(authOptions);
 
-  if (!session) {
-    redirect("/api/auth/signin?callbackUrl=/add-product");
-  }
+    if (!session) {
+        redirect("/api/auth/signin?callbackUrl=/add-product");
+    }
 
-  return (
-    <div>
-      <h1 className="mb-3 text-lg font-bold">Add Product</h1>
-      <form action={addProduct}>
-        <input
-          required
-          name="name"
-          placeholder="Name"
-          className="input-bordered input mb-3 w-full"
-        />
-        <textarea
-          required
-          name="description"
-          placeholder="Description"
-          className="textarea-bordered textarea mb-3 w-full"
-        />
-        <input
-          required
-          name="imageUrl"
-          placeholder="Image URL"
-          type="url"
-          className="input-bordered input mb-3 w-full"
-        />
-        <input
-          required
-          name="price"
-          placeholder="Price"
-          type="number"
-          className="input-bordered input mb-3 w-full"
-        />
-        <FormSubmitButton className="btn-block">Add Product</FormSubmitButton>
-      </form>
-    </div>
-  );
+    return (
+        <div>
+            <h1 className="mb-3 text-lg font-bold">Add Product</h1>
+            <form action={addProduct}>
+                <input
+                    required
+                    name="name"
+                    placeholder="Name"
+                    className="input-bordered input mb-3 w-full"
+                />
+                <textarea
+                    required
+                    name="description"
+                    placeholder="Description"
+                    className="textarea-bordered textarea mb-3 w-full"
+                />
+                <input
+                    required
+                    name="fileUrl"
+                    placeholder="File URL"
+                    type="url"
+                    className="input-bordered input mb-3 w-full"
+                />
+                <input
+                    required
+                    name="imageUrl"
+                    placeholder="Image URL"
+                    type="url"
+                    className="input-bordered input mb-3 w-full"
+                />
+                <input
+                    required
+                    name="price"
+                    placeholder="Price"
+                    type="number"
+                    className="input-bordered input mb-3 w-full"
+                />
+                <FormSubmitButton className="btn-block">Add Product</FormSubmitButton>
+            </form>
+        </div>
+    );
 }
